e74bdae62a2b7fc34f6ef4b336f0679a054b7484
Carlos <carloselima2001@gmail.com>
2024-06-27 19:49:38 -0300

    fix database connection

---
+++ Models/User.cs
@@ -0,0 +1,8 @@
+﻿namespace CrudDapper.Models
+{
+    public class User
+    {
+        public int Id { get; set; }
+        public string Users { get; set; } = string.Empty;
+    }
+}
---
+++ Services/UserService/UserService.cs
@@ -0,0 +1,13 @@
+﻿namespace CrudDapper.Services.UserService
+{
+    public class UserService : IUserService
+    {
+        private readonly IConfiguration _configuration;
+        private readonly string getConnection;
+        public UserService(IConfiguration configuration) 
+        { 
+            _configuration = configuration;
+            getConnection = _configuration.GetConnectionString("DefaultConnection");
+        }
+    }
+}
